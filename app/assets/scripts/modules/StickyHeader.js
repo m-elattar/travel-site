848b83a7691a1de826f733c0f3416dee647bbf4e
m-elattar <civil.e.mahmoud@gmail.com>
2018-03-15 14:41:29 +0200

    finished sticky headers & scrolling

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,12 +1,21 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class stickyheader {
 	constructor(){
 		this.siteheader= $('.site-header');
 		this.HeaderTriggerElement = $('.large-hero__title');
 		this.CreateHeaderWaypoint();
+		this.PageSection=$('.page-section');
+		this.HeaderLinks = $('.primary-nav a');
+		this.CreatePageSectionWaypoint();
+		this.addSmoothScroll();
 	}
+
+	addSmoothScroll(){
+		this.HeaderLinks.smoothScroll();
+	};
 
 	CreateHeaderWaypoint(){
 		var that = this ;
@@ -19,6 +28,35 @@ class stickyheader {
 					that.siteheader.removeClass('site-header--dark');
 				}
 			}
+		});
+	}
+
+	CreatePageSectionWaypoint(){
+		var that = this; 
+		this.PageSection.each(function(){
+			var currentsection = this;
+			new Waypoint({
+				element: currentsection ,
+				handler: function(direction){
+					if(direction=='down'){
+						var matchingHeaderLink = currentsection.getAttribute('data-matching-link');
+					that.HeaderLinks.removeClass('is-current-link');
+					$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset : '18%'
+			});
+			new Waypoint({
+				element: currentsection ,
+				handler: function(direction){
+					if(direction=='up'){
+						var matchingHeaderLink = currentsection.getAttribute('data-matching-link');
+					that.HeaderLinks.removeClass('is-current-link');
+					$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset : '-40%'
+			});
 		});
 	}
 }
